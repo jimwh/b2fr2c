ee66aae48d01f8e938fc7a2b1d9bf5916b7b5923
jh3389 <jh3389@columbia.edu>
2015-09-03 16:51:32 -0400

    zip file

--- src/main/java/edu/columbia/rascal/cumc/Extractor.java
+++ src/main/java/edu/columbia/rascal/cumc/Extractor.java
@@ -1,5 +1,6 @@
 package edu.columbia.rascal.cumc;
 
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +8,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @Component
 public class Extractor {
@@ -40,6 +41,7 @@ public class Extractor {
     @Autowired
     public Extractor(JdbcTemplate jt) {
         this.jdbcTemplate = jt;
+        System.setProperty("user.dir", "/tmp");
     }
 
     public void start() {
@@ -52,6 +54,11 @@ public class Extractor {
             }
         }
         extractMapper();
+        try {
+            zipFiles();
+        } catch (IOException e) {
+            log.error("caught err:", e);
+        }
     }
 
     private void extractMapper() {
@@ -80,6 +87,45 @@ public class Extractor {
             os.close();
         } catch (Exception e) {
             log.error("caught: ", e);
+        }
+    }
+
+    private void zipFiles() throws IOException {
+        File dir = new File(RootDir);
+        DateTime dateTime = DateTime.now();
+        String zipFileName = "/tmp/rascal_to_cumc_" + dateTime.toString("yyyyMMdd") + ".zip";
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName));
+        addDir(dir, out);
+        out.close();
+    }
+
+    private void addDir(File dir, ZipOutputStream out) throws IOException {
+        File[] files = dir.listFiles();
+        byte[] buf = new byte[1024];
+
+        for(File f : files) {
+            if (f.isDirectory()) {
+                addDir(f, out);
+                continue;
+            }
+            /*
+            FileInputStream in = new FileInputStream(f.getAbsolutePath());
+            out.putNextEntry(new ZipEntry(f.getAbsolutePath()));
+
+            FileInputStream in = new FileInputStream(f.getCanonicalPath());
+            out.putNextEntry(new ZipEntry(f.getCanonicalPath()));
+
+            */
+
+            FileInputStream in = new FileInputStream(f.getCanonicalFile());
+            out.putNextEntry(new ZipEntry(f.getCanonicalPath()));
+
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            out.closeEntry();
+            in.close();
         }
     }
 
